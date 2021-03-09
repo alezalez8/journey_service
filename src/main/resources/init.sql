db318d892dbc44da711a9930cf8f2c5a5361a25b
alezalez8 <5446943@ukr.net>
2021-03-09 23:33:02 +0200

    HW-lesson2: add connect to db

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,10 +1,24 @@
+drop table IF EXISTS schedule_service;
+
 create table schedule_service (
 id BIGSERIAL,
-city_from VARCHAR (50),
-city_to  VARCHAR (50),
-departure_time DATE ,
-arrival_time DATA ,
-);
+city_from VARCHAR (50) NOT NULL,
+city_to  VARCHAR (50) NOT NULL,
+departure_time DATE  NOT NULL,
+arrival_time DATE NOT NULL );
 
 insert into schedule_service (id, city_from, city_to, departure_time, arrival_time)
-values
+values ('Odessa', 'Kiev', '09.40', '20.15' );
+values ('Lviv', 'Kiev', '09.40', '20.15' );
+values ('Odessa', 'Kiev', '09.40', '20.15' );
+values ('Odessa', 'Kiev', '09.40', '20.15' );
+values ('Odessa', 'Kiev', '09.40', '20.15' );
+values ('Odessa', 'Kiev', '09.40', '20.15' );
+values ('Odessa', 'Kiev', '09.40', '20.15' );
+values ('Odessa', 'Kiev', '09.40', '20.15' );
+values ('Odessa', 'Kiev', '09.40', '20.15' );
+values ('Odessa', 'Kiev', '09.40', '20.15' );
+values ('Odessa', 'Kiev', '09.40', '20.15' );
+values ('Odessa', 'Kiev', '09.40', '20.15' );
+
+select * from schedule_service;
