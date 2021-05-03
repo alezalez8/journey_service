97975ead155572ccaabfe823c34e5ebb69833b05
alezalez8 <5446943@ukr.net>
2021-05-03 15:32:46 +0300

    first save hw5

--- src/main/java/org/hillel/persistence/repository/GenericRepository.java
+++ src/main/java/org/hillel/persistence/repository/GenericRepository.java
@@ -11,4 +11,17 @@ public interface GenericRepository<E, ID> {
     void removeById(ID id);
 
     void remove(E entity);
+
+    // add methods for hw5:
+
+    /*
+    * 1. findAll (метод поиска через HQL)
+2. findAllAsNative (метод поиска через вызов sql запроса)
+3. findAllAsNamed (метод поиска через вызов именованного запроса по алиасу)
+4. findAllAsCriteria (метод поиска через CriteriaBuilder)
+5. findAllAsStoredProcedure (метод поиска через вызов хранимой функции)
+
+То есть для каждой сущности должен быть свой repository и service классы,
+в рамках которых вы реализуете все требуемые методы.
+    * */
 }
